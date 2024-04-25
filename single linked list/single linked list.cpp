0b4800d5683d45da4898d474d8273e904f4d7836
sabilafiridas <sabilafirdias@gmail.com>
2024-04-25 10:14:59 +0700

    membuat kondisi if start sama dengan null

--- single linked list/single linked list.cpp
+++ single linked list/single linked list.cpp
@@ -20,7 +20,12 @@ void addNode() {
     cin >> nim;
     cout << "Masukkan Nama: ";
     cin >> nama;
-    
+    nodeBaru->noMhs = nim;
+    nodeBaru->name = nama;
+
+    if (START == NULL || nim <= START->noMhs) {
+        
+    }
 }
 
 int main()

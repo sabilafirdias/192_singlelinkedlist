cb452df7f42108826c07577f521a18588daf80a2
sabilafiridas <sabilafirdias@gmail.com>
2024-04-25 09:26:45 +0700

    Add project files.

---
+++ single linked list/single linked list.cpp
@@ -0,0 +1,15 @@
+// single linked list.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+struct node {
+   
+};
+
+int main()
+{
+    
+}
+

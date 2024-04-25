62f9435c28d52520ee2a2250b6a380e376644943
sabilafiridas <sabilafirdias@gmail.com>
2024-04-25 10:46:54 +0700

    membuat operasi deleteNode

--- single linked list/single linked list.cpp
+++ single linked list/single linked list.cpp
@@ -61,6 +61,16 @@ bool searchNode(int nim, Node* current, Node* previous) {
     if (current == NULL) {
         return false;
     }
+    else if (current->noMhs == nim) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool deleteNode(int nim) {
+    
 }
 
 int main()
